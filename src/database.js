2a71b7ad866d5241e58a5b511eac66d7bc4418ad
Unknown <jc.arredondomurillo@ugto.mx>
2019-12-04 17:27:08 -0600

    MDB

--- src/database.js
+++ src/database.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost/node-notes-db', {
-  useCreateIndex: true,
-  useNewUrlParser: true
-})
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.error(err));
+//en local
+//mongoose.set('useFindAndModify', false);
+//mongoose.connect('mongodb://localhost/node-notes-db', {
+ // useCreateIndex: true,
+//  useNewUrlParser: true
+//})
+//  .then(db => console.log('DB is connected'))
+//  .catch(err => console.error(err));
+
+//en docker
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
